48fd81336d0c824011f1672939dbd622226a4f8e
otavio <otavioadamis@gmail.com>
2024-03-22 15:37:36 -0300

    simplificando-banco

---
+++ UniforBackend.DAL/Migrations/20240322175605_migration-#6-simplifiquei-banco.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniforBackend.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration6simplifiqueibanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarrinhoItem");
+
+            migrationBuilder.DropTable(
+                name: "CompraItem");
+
+            migrationBuilder.DropTable(
+                name: "Carrinhos");
+
+            migrationBuilder.DropColumn(
+                name: "MetodoPagamento",
+                table: "Compras");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Compras");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ItemId",
+                table: "Compras",
+                type: "varchar(36)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compras_ItemId",
+                table: "Compras",
+                column: "ItemId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Compras_Itens_ItemId",
+                table: "Compras",
+                column: "ItemId",
+                principalTable: "Itens",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Compras_Itens_ItemId",
+                table: "Compras");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Compras_ItemId",
+                table: "Compras");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "Compras");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MetodoPagamento",
+                table: "Compras",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Compras",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Carrinhos",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    UserId = table.Column<string>(type: "varchar(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrinhos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carrinhos_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompraItem",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", nullable: false),
+                    CompraId = table.Column<string>(type: "varchar(36)", nullable: false),
+                    ItemId = table.Column<string>(type: "varchar(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompraItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompraItem_Compras_CompraId",
+                        column: x => x.CompraId,
+                        principalTable: "Compras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompraItem_Itens_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Itens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarrinhoItem",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", nullable: false),
+                    CarrinhoId = table.Column<string>(type: "varchar(36)", nullable: false),
+                    ItemId = table.Column<string>(type: "varchar(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoItem_Carrinhos_CarrinhoId",
+                        column: x => x.CarrinhoId,
+                        principalTable: "Carrinhos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoItem_Itens_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Itens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoItem_CarrinhoId",
+                table: "CarrinhoItem",
+                column: "CarrinhoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoItem_ItemId",
+                table: "CarrinhoItem",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carrinhos_UserId",
+                table: "Carrinhos",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompraItem_CompraId",
+                table: "CompraItem",
+                column: "CompraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompraItem_ItemId",
+                table: "CompraItem",
+                column: "ItemId");
+        }
+    }
+}
