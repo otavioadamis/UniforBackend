0aa2cd3233278e382d4900d7e8eb423d9bb2391e
rafaelsalb <rafael.silva.albuquerque@hotmail.com>
2024-03-30 13:35:22 -0300

    correção do tipo das respostas + método GetById, que retorna um User, para autenticação

--- UniforBackend.API/Controllers/UserController.cs
+++ UniforBackend.API/Controllers/UserController.cs
@@ -25,7 +25,7 @@ namespace UniforBackend.API.Controllers
         }
 
         [HttpPost()]
-        public ActionResult<User> Signup(PostUserDTO user)
+        public ActionResult<LoginResponseModel> Signup(PostUserDTO user)
         {
             var addedUser = _userService.Signup(user);
             return Ok(addedUser);
