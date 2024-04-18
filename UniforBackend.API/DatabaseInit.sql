191b70b2451856afca496f291f27f89a389766bb
otavio <otavioadamis@gmail.com>
2024-04-18 16:39:00 -0300

    adicionado mais categorias e subcategorias (olhar trello)

--- UniforBackend.API/DatabaseInit.sql
+++ UniforBackend.API/DatabaseInit.sql
@@ -1,3 +1,5 @@
+--Roupas
+
 INSERT INTO "Categorias"(
 	"Id", "Nome")
 	VALUES ('1', 'Roupas');
@@ -9,6 +11,60 @@ INSERT INTO "SubCategorias"(
 INSERT INTO "SubCategorias"(
 	"Id", "Nome", "CategoriaId")
 	VALUES ('2', 'Roupas Femininas', '1');
+
+--Acessorios
+
+INSERT INTO "Categorias"(
+	"Id", "Nome")
+	VALUES ('2', 'Acessorios');
+
+INSERT INTO "SubCategorias"(
+	"Id", "Nome", "CategoriaId")
+	VALUES ('3', 'Acessorios Bijouteria', '2');
+
+INSERT INTO "SubCategorias"(
+	"Id", "Nome", "CategoriaId")
+	VALUES ('4', 'Acessorios Bolsas', '2');
+
+--Eletronicos
+
+INSERT INTO "Categorias"(
+	"Id", "Nome")
+	VALUES ('3', 'Eletronicos');
+
+INSERT INTO "SubCategorias"(
+	"Id", "Nome", "CategoriaId")
+	VALUES ('5', 'Eletronicos', '3');
+
+--Livros
+
+INSERT INTO "Categorias"(
+	"Id", "Nome")
+	VALUES ('4', 'Livros');
+
+INSERT INTO "SubCategorias"(
+	"Id", "Nome", "CategoriaId")
+	VALUES ('6', 'Livros', '4');
+
+--Instrumentos
+
+INSERT INTO "Categorias"(
+	"Id", "Nome")
+	VALUES ('5', 'Instrumentos');
+
+INSERT INTO "SubCategorias"(
+	"Id", "Nome", "CategoriaId")
+	VALUES ('7', 'Instrumentos', '5');
+
+--Artigos de casa
+
+INSERT INTO "Categorias"(
+	"Id", "Nome")
+	VALUES ('6', 'Artigos Casa');
+
+INSERT INTO "SubCategorias"(
+	"Id", "Nome", "CategoriaId")
+	VALUES ('8', 'Artigos Casa', '6');
 
 INSERT INTO "Users"(
 	"Id", "Nome", "Email", "Matricula", "Foto", "Password", "Tipo", "Contato", "CriadoEm")
