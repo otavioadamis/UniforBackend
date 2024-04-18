f104c5755b52755773a470eb0975713d49aa76b9
otavio <otavioadamis@gmail.com>
2024-04-18 12:20:49 -0300

    .com no admin e ajuste nome das subcateg

--- UniforBackend.API/DatabaseInit.sql
+++ UniforBackend.API/DatabaseInit.sql
@@ -4,12 +4,12 @@ INSERT INTO "Categorias"(
 
 INSERT INTO "SubCategorias"(
 	"Id", "Nome", "CategoriaId")
-	VALUES ('1', 'Masculino', '1');
+	VALUES ('1', 'Roupas Masculinas', '1');
 
 INSERT INTO "SubCategorias"(
 	"Id", "Nome", "CategoriaId")
-	VALUES ('2', 'Feminino', '1');
+	VALUES ('2', 'Roupas Femininas', '1');
 
 INSERT INTO "Users"(
 	"Id", "Nome", "Email", "Matricula", "Foto", "Password", "Tipo", "Contato", "CriadoEm")
-	VALUES ('Admin1', 'UsuarioAdminSQLscript', 'admin@admin', '123456789', '', '$2a$11$AFDxQBUYbazjRGIRW9yrQe4lIzfKoHWgd1OSWT9D6oBiQsC/Dhq36', 1, '123456789', CURRENT_TIMESTAMP);
+	VALUES ('Admin1', 'UsuarioAdminSQLscript', 'admin@admin.com', '123456789', '', '$2a$11$AFDxQBUYbazjRGIRW9yrQe4lIzfKoHWgd1OSWT9D6oBiQsC/Dhq36', 1, '123456789', CURRENT_TIMESTAMP);
